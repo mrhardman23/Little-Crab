f569ceebc11e7201a01778026d57a0d9467a5a18
Mr. Hardman <scott.hardman23@gmail.com>
2018-10-11 13:30:21 -0500

    Starting scenario
    Starting scenario for the teaching of classes

---
+++ Program - 3 little-crab/Crab.java
@@ -0,0 +1,13 @@
+import greenfoot.*;
+
+/**
+ * This class defines a crab. Crabs live on the beach.
+ */
+public class Crab extends Actor
+{
+    public void act()
+    {
+        // Add your action code here
+        move(2);
+    }
+}
---
+++ Program - 3 little-crab/CrabWorld.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (Actor, World, Greenfoot, GreenfootImage)
+
+public class CrabWorld extends World
+{
+    /**
+     * Create the crab world (the beach). Our world has a size 
+     * of 560x560 cells, where every cell is just 1 pixel.
+     */
+    public CrabWorld() 
+    {
+        super(560, 560, 1);
+    }
+}
